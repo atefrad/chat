f0c50aac6d38cded72ec28efc7edbf3d3f6df411
Fateme Atefrad <fa.atefrad@gmail.com>
2024-02-02 17:04:41 +0330

    first commit

---
+++ src/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Http\Controllers;
+class AuthenticationController
+{
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function checkLogin()
+    {
+
+    }
+}
---
+++ views/register.view.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>register</title>
+</head>
+<body>
+    <div class="container">
+        <h3 class="mt-4">Register form</h3>
+        <form action="" method="post">
+            <div class="my-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="username">
+            </div>
+
+            <div class="my-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="email">
+            </div>
+
+            <div class="my-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="name">
+            </div>
+
+            <div class="my-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="password">
+            </div>
+
+            <div class="my-3">
+                <input type="submit" class="btn btn-success" name="submit" value="register">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
